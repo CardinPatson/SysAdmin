8079c9fb939e184fae0b6b19fdf88b7f5e0599cb
CardinPatson <cardin.tiako@gmail.com>
2022-04-14 23:25:54 +0200

    config mail

--- mail/db/db.sql
+++ mail/db/db.sql
@@ -1,24 +1,35 @@
-create database maibox;
-
-GRANT ALL PRIVILEGES ON mailbox.* to 'mailUser'@'localhost' identified by 'root';
+create database mailbox;
+CREATE USER 'woody'@'localhost' IDENTIFIED BY 'hypersecurise';
+GRANT ALL PRIVILEGES ON mailbox.* to 'woody'@'localhost' WITH GRANT OPTION ;
 
 FLUSH PRIVILEGES;
 
 use mailbox;
 
-CREATE TABLE 'domains' (
-`id` INT NOT NULL AUTO_INCREMENT,
-`domain_name` VARCHAR(50) NOT NULL,
-PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- CREATE TABLE 'domains' (
+-- `id` INT NOT NULL AUTO_INCREMENT,
+-- `domain_name` VARCHAR(50) NOT NULL,
+-- PRIMARY KEY (`id`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE 'users' (
 `id` INT NOT NULL AUTO_INCREMENT,
-`domain_id` INT NOT NULL,
+`username` VARCHAR(150) NOT NULL,
 `password` VARCHAR(106) NOT NULL,
-`email` VARCHAR(120) NOT NULL,
-`maildir` VARCHAR(120) NOT NULL,
+`domain` VARCHAR(50) NOT NULL,
+`foldermail` VARCHAR(150) NOT NULL,
 PRIMARY KEY (`id`),
-UNIQUE KEY `email` (`email`),
-FOREIGN KEY (domain_id) REFERENCES domains(id) ON DELETE CASCADE
+UNIQUE KEY `username` (`username`)
+-- FOREIGN KEY (domain_id) REFERENCES domains(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- create table users(
+-- id INT NOT NULL AUTO_INCREMENT,
+-- username VARCHAR(150) NOT NULL,
+-- password VARCHAR(150) NOT NULL,
+-- domain VARCHAR(50) NOT NULL,
+-- foldermail VARCHAR(150) NOT NULL,
+-- PRIMARY KEY(id),
+-- UNIQUE KEY username(username)
+-- );
--- mail/db/users.sql
+++ mail/db/users.sql
@@ -1,16 +1,20 @@
-INSERT INTO 'mailbox'.'domains'
-('id' ,'domain_name')
-VALUES
-('1', 'm1-3.ephec-ti.be');
+-- INSERT INTO 'mailbox'.'domains'
+-- ('id' ,'domain_name')
+-- VALUES
+-- ('1', 'm1-3.ephec-ti.be');
 
 INSERT INTO 'mailbox'.'users'
-('id', 'domain_id', 'password' , 'email', 'maildir')
+('id', 'username' , 'password' ,  'domain', 'foldermail')
 VALUES
-('1', '1', ENCRYPT('password1', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'cardin@m1-3.ephec-ti.be', 'm1-3.ephec-ti.be/cardin/');
+('1','cardin@m1-3.ephec-ti.be', ENCRYPT('password1', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))) ,'m1-3.ephec-ti.be', 'm1-3.ephec-ti.be/cardin/');
 
 INSERT INTO 'mailbox'.'users'
-('id', 'domain_id', 'password' , 'email', 'maildir')
+('id', 'username', 'password' , 'domain',  'foldermail')
 VALUES
-('2', '1', ENCRYPT('password2', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))), 'marina@m1-3.ephec-ti.be', 'm1-3.ephec-ti.be/marina/');
+('2', 'marina@m1-3.ephec-ti.be',ENCRYPT('password2', CONCAT('$6$', SUBSTRING(SHA(RAND()), -16))),'m1-3.ephec-ti.be',   'm1-3.ephec-ti.be/marina/');
 
 
+-- insert into users
+-- (id, username, password , domain,foldermail)
+-- values
+-- (1,'cardin@m1-3.ephec-ti.be', 'hypersecurise', 'm1-3.ephec-ti.be', 'm1-3.ephec-ti.be/cardin/')
