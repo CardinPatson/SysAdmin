253d0a560590cd85fadf1cb9950ba38b3b5df406
Patson <cardin.tiako@gmail.com>
2022-05-10 14:59:32 +0200

    service interne

---
+++ services_internes/intranet/index.php
@@ -0,0 +1,12 @@
+<!-- Page html de l'intranet -->
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css">
+    <title>WoodyToys</title>
+</head>
+<body>
+   <p>Bienvenue sur l'intranet de woody toy</p>
+</body>
